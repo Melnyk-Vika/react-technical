7830a1af79ab47985d1a81dd4d3baa563b10c560
Melnyk Viktoriia <vikavmelnyk@gmail.com>
2023-07-09 23:21:58 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persist}>
-        <BrowserRouter basename="/goit-react-technical">
+        <BrowserRouter basename="/react-technical">
           <App />
         </BrowserRouter>
       </PersistGate>
